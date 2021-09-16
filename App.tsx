86af83f124e945b7cc9c16979e2eac374b1bea0e
BARHOUNI <younes-barhouni@users.noreply.github.com>
2021-09-16 10:19:22 +0200

    Add main index and App entries

---
+++ App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Main React Native App
+ *
+ * @format
+ * @flow strict-local
+ */
+import React, { useEffect, useState } from 'react';
+
+
+
+const App: () => any = () => {
+
+  useEffect(() => {
+    
+  }, []);
+
+  
+  return (
+    <>
+    hello
+    </>
+  );
+};
+
+
+
+export default App;
