509e7b7c9411cd46821bccc56b20ea7eb3566bb6
BARHOUNI <younes-barhouni@users.noreply.github.com>
2021-09-20 16:28:35 +0200

    Add colors constant

---
+++ app/bluetooth/constants/colors.ts
@@ -0,0 +1,26 @@
+/* CSS HEX */
+// --xiketic: #03071eff;
+// --dark-sienna: #370617ff;
+// --rosewood: #6a040fff;
+// --dark-red: #9d0208ff;
+// --rosso-corsa: #d00000ff;
+// --vermilion: #dc2f02ff;
+// --persimmon: #e85d04ff;
+// --carrot-orange: #f48c06ff;
+// --orange-web: #faa307ff;
+// --selective-yellow: #ffba08ff;
+
+const COLORS = {
+    $_xiketic: '#03071eff',
+    $_dark_sienna: '#370617ff',
+    $_rosewood: '#6a040fff',
+    $_dark_red: '#9d0208ff',
+    $_rosso_corsa: '#d00000ff',
+    $_vermilion: '#dc2f02ff',
+    $_persimmon: '#e85d04ff',
+    $_carrot_orange: '#f48c06ff',
+    $_orange_web: '#faa307ff',
+    $_selective_yellow: '#ffba08ff',
+  };
+  
+export default COLORS;
